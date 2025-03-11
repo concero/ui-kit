b43b071b7532c301c9e5eb9f786460ab14ef0750
mikhail-concero <mikhail@concero.io>
2025-03-11 15:42:53 +0300

    Update deps, and including fonts for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-vite'
-
+import path from 'path'
+import { normalizePath } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	addons: [
@@ -8,6 +10,33 @@ const config: StorybookConfig = {
 		'@chromatic-com/storybook',
 		'@storybook/addon-interactions',
 	],
+	core: {
+		builder: '@storybook/builder-vite',
+	},
+	async viteFinal(config) {
+		if (config && config.resolve) {
+			config.resolve.alias = {
+				...config.resolve.alias,
+				'@fonts': path.resolve(__dirname, '../src/lib/assets/fonts/DM_Sans'),
+			}
+			config.plugins = [
+				...(config.plugins || []),
+				viteStaticCopy({
+					targets: [
+						{
+							src: normalizePath(
+								path.resolve(__dirname, '..', 'src', 'lib', 'assets', 'fonts/DM_Sans/*'),
+							),
+							dest: 'assets/fonts',
+						},
+					],
+				}),
+			]
+			return config
+		} else {
+			return config
+		}
+	},
 	framework: {
 		name: '@storybook/react-vite',
 		options: {},
